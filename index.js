a1a09ff5b2dcd32e0f8c18d9ae7358738e78a409
Philipp Häusle <philipp.haeusle@hotmail.com>
2018-12-07 00:34:09 +0100

    added first steps for loss

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let csvjson = require('csvjson');
 let path = require('path');
 
 let networkBandwidth=40;
-let budget=37;//budget in euro
+let budget=2;//budget in euro
 
 let inputPrice='./assignments/price.csv';
 let inputTime='./assignments/task_time_instance.csv';
@@ -69,7 +69,7 @@ const costs=(fromID, toID)=>{
 };
 
 const takesTime=(id, instance)=>{
-    let tmp;
+    let tmp=undefined;
    time.forEach(function(t){
        if(t.task==id){
            tmp = t[instance];
@@ -153,6 +153,7 @@ const checkIfTaskCompleted=(id,serverAssignments)=>{
     return found;
 };
 
+//implements the assign algorithm from https://en.wikipedia.org/wiki/Heterogeneous_Earliest_Finish_Time
 function assignToBestWorker(task, serverAssignments) {
     let mustWaitFor=isDependentOn(task);
     let mustWaitForArray=[];
@@ -170,7 +171,6 @@ function assignToBestWorker(task, serverAssignments) {
         return undefined
     }
 
-    //let computationTime=[]; //times how long the computation takes for each server
     let availableList=serverAssignments.serverList;
     let bestStartTime=9999999999;
     let bestFinishTime=9999999999;
@@ -273,14 +273,15 @@ const assignToWorkers=(taskList)=>{
                 console.log("HAVE: "+arr);
             }
         });
-        console.log(temp);
         tempList=temp;
-    };
-    if(!loop){
-        ///TODO: maxbe increase all start times
-        console.log('could not assign all tasks');
-
+        if(!loop){
+            console.log('could not assign all tasks, waiting..');
+            serverList.forEach(function(t){
+                t+=0.1;  //increase waiting time + 0.1s
+            })
+        }
     }
+
     let arr=[];
     serverAssignments.completedTasks.forEach(function(x){
         arr.push(x.taskId);
@@ -288,13 +289,89 @@ const assignToWorkers=(taskList)=>{
     return serverAssignments;
 };
 
+//HERE THE FUNCTIONS FOR LOSS START
+
+const executionCosts=(task,instance)=>{
+    let time=takesTime(task,instance);
+    let hourCost=price[instance].Price;
+    let costsSecond=hourCost/3600;
+    return parseFloat(costsSecond*time);
+};
+
+const lossWeight=(task,oldInstance,newInstance)=>{
+    let a=takesTime(task,newInstance);
+    let b=takesTime(task,oldInstance);
+    let c=executionCosts(task,oldInstance);
+    let d=executionCosts(task,newInstance);
+
+    if(c<=d){
+        return 0;
+    }
+    return (a-b)/(c-d)
+};
+
+const reschedule=(taskId, newServer,data)=>{
+/*TODO:  rescheule task from one server to the other
+* */
+};
+
+const lossAlgorithm=(data)=>{
+    let costs=currentCostsForServers(data.serverList).total;
+    if(costs <= budget){
+        return data;
+    }
+    //budget is exceeded
+    //Build an array A[num_tasks][num_machines]
+    let array=[data.completedTasks.length];
+    for(let i=0; i<data.completedTasks.length; i++){
+        array[i]=[data.serverList.length];
+        for(let j=0; j<data.serverList.length; j++){
+            if(data.completedTasks[i].serverId===j){
+                array[i][j]=0;
+            }else{
+                array[i][j]=lossWeight(data.completedTasks[i].taskId,data.completedTasks[i].serverId,j);
+            }
+        }
+    }
+    let condition=costs>budget;
+    let changed=true;
+    //while condition and not all reassignments tried
+    while(condition && changed){
+        let low=undefined;
+        let currI=0;
+        let currJ=0;
+        changed=false;
+        for(let i=0; i<array.length; i++) {
+            for (let j = 0; j < array[i].length; j++) {
+                if(array[i][j]!==0 && (low ===undefined || array[i][j] < low)){
+                    changed=true;
+                    low=array[i][j];
+                    currI=i;
+                    currJ=j;
+                }
+            }
+        }
+        if(changed){
+            //reschedule task i to machine j
+            reschedule(currI,currJ,data);
+            costs=currentCostsForServers(data.serverList).total;
+            condition=costs>budget;
+        }
+    }
+    if(!changed){
+        console.log("ERROR while loss. could not find better solution")
+    }
+    return data;
+};
+
+
 let result=assignToWorkers(prioritize());
 console.log("Makespan: "+calcMakespan(result.serverList)+" seconds");
 console.table(result.completedTasks);
 console.log("Total cost: "+ currentCostsForServers(result.serverList).total);
 console.table(currentCostsForServers(result.serverList).single);
 
-//console.table(assignToWorkers(prioritize()));
+lossAlgorithm(result);
 
 
 
