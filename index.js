64231867f60999f04dd7bbead34c5a1e9926867e
Philipp Häusle <philipp.haeusle@hotmail.com>
2018-12-06 20:36:56 +0100

    added prioritizing algorithm

--- index.js
+++ index.js
@@ -22,8 +22,26 @@ let price=csvjson.toObject(dataPrice, options);
 let time=csvjson.toObject(dataTime, options);
 let traffic=csvjson.toObject(dataTraffic, options);
 
-
+//this function returns an array of task ids, that need the result of task with (id)
 const dependsOn=(id)=>{
+    let result=[];
+    let trafficCopy=JSON.parse(JSON.stringify(traffic));
+    trafficCopy.forEach(function (d){
+        if(d.task===id){
+            delete d.task;
+            for (key in d){
+                if(d[key]!==''){
+                    result.push(key);
+                }
+            }
+        }
+
+    });
+    return result;
+};
+
+//this function returns an array of task ids, that are must be finished before the task with(id) can start
+const isDependentOn=(id)=>{
     let result=[];
     let trafficCopy=JSON.parse(JSON.stringify(traffic));
     trafficCopy.forEach(function (d){
@@ -46,7 +64,8 @@ const costs=(fromID, toID)=>{
             temp=d;
         }
     });
-    return temp[toID] / 40; //in seconds
+    let size=temp[toID];
+    return size / networkBandwidth; //in seconds
 };
 
 const takesTime=(id, instance)=>{
@@ -56,7 +75,7 @@ const takesTime=(id, instance)=>{
            tmp = t[instance];
        }
    });
-   return tmp;
+   return parseFloat(tmp);
 };
 
 const getAllTasks=()=>{
@@ -74,6 +93,44 @@ const getAllInstances=()=>{
     });
     return tmp;
 };
+
+
+const averageComputationCostAmongAllNodes=(id)=> {
+    let count=0;
+    price.forEach(function (instance){
+        count+=takesTime(id,instance.id);
+    });
+    return (count/price.length);
+};
+
+//conputes the Cij from Prioritizing tasks (https://en.wikipedia.org/wiki/Heterogeneous_Earliest_Finish_Time)
+const computAverageComputationCostsAmongAllNodes=(i, j)=> {
+    let all=0;
+    for(let t=0; t<=price.length-1;t++){
+        all=all+costs(i,j);
+    }
+    return all/price.length;
+};
+
+//implements the prioritizing algorithm from https://en.wikipedia.org/wiki/Heterogeneous_Earliest_Finish_Time
+//task is an ID of a task
+const computePriority=(task)=>{
+    let w=averageComputationCostAmongAllNodes(task);
+    let succ=dependsOn(task);
+    let i=task;
+
+    let max=0;
+    succ.forEach(function(j){
+        let c=computAverageComputationCostsAmongAllNodes(i,j);
+        let currentCost=c+computePriority(j);
+        if(max<currentCost){
+            max=currentCost;
+        }
+    });
+    return w+max;
+};
+
+
 
 const comuteRandomSchedule=()=>{
     let timeCopy=JSON.parse(JSON.stringify(time));
@@ -95,10 +152,29 @@ const comuteRandomSchedule=()=>{
         }
     }
 
+};
+
+//returns a prioritized and sorted list of tasks
+const prioritize=()=>{
+    let result=[];
+    getAllTasks().forEach(function(t){
+        result.push({
+            priorityScore: computePriority(t),
+            id:t
+        });
+    });
+    result.sort((a, b) => b.priorityScore - a.priorityScore);
+    return result;
+};
+
+//the function that assigns sorted task list to workers;
+const assignToWorkers=(taskList)=>{
 
 };
 
-comuteRandomSchedule();
+assignToWorkers(prioritize());
+
+
 
 
 
